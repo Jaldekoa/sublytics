1c54afe94705382f451b95c27a73ab58aaf3e437
Jon Aldekoa <jaldekoa@gmail.com>
2026-03-05 23:32:55 +0100

    Add modal management

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -24,3 +24,43 @@ $menuBtn.addEventListener('click', () => {
         $menuBtn.setAttribute('aria-expanded', 'false');
     }
 });
+
+// Modal management
+const $modal = document.querySelector('.nav__dialog');
+const $form = document.querySelector('.dialog__form');
+
+$form.addEventListener('submit', () => {
+    const formData = new FormData($form);
+    const data = Object.fromEntries(formData.entries());
+
+    const title = data.title;
+    const category = data.category;
+    const price = Number(data.price).toLocaleString('es-ES');
+    const period = data.period;
+
+    const $app = document.querySelector('.app');
+    const $newCard = document.createElement('article');
+    $newCard.classList.add('card');
+
+    console.log(typeof price,)
+
+    $newCard.innerHTML = `
+        <a class="card-link" href="/subscription.html?title=${title}&category=${category}&price=${price}&period=${period}">
+            <img class="card-image" src="assets/${title}.webp" alt="Suscripción a ${title}" />
+            
+            <div class="card-content">
+                <div class="card-info">
+                    <h2 class="card-title">${title}</h2>
+                    <p class="card-category">${category}</p>
+                </div>
+                <div class="card-pricing">
+                    <p class="card-price">${price}€</p>
+                    <p class="card-period">${period}</p>
+                </div>
+            </div>
+        </div>
+    `;
+
+    $app.append($newCard);
+    $form.reset();
+});
