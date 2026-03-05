b057fd99a697bf76aed7e90ec8b35f21bea48c30
Jon Aldekoa <jaldekoa@gmail.com>
2026-03-05 20:21:56 +0100

    Rename DOM elements

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -3,24 +3,24 @@ window.addEventListener('DOMContentLoaded', () => {
     const params = new URLSearchParams(window.location.search);
     const user = params.get('user');
 
-    const usernameEl = document.querySelector(".username")
-    usernameEl.textContent = user ? `Bienvenido, ${user}` : `Bienvenido`;
+    const $username = document.querySelector(".username")
+    $username.textContent = user ? `Bienvenido, ${user}` : `Bienvenido`;
 });
 
 // Menu management
-const menuBtn = document.querySelector('.btn-menu');
-const mainNav = document.getElementById('main-navigation');
+const $menuBtn = document.querySelector('.btn-menu');
+const $mainNav = document.getElementById('main-navigation');
 
 menuBtn.addEventListener('click', () => {
     const isHidden = mainNav.hidden;
 
     if (isHidden) {
-        mainNav.hidden = false;
-        menuBtn.textContent = "close";
-        menuBtn.setAttribute('aria-expanded', 'true');
+        $mainNav.hidden = false;
+        $menuBtn.textContent = "close";
+        $menuBtn.setAttribute('aria-expanded', 'true');
     } else {
-        mainNav.hidden = true;
-        menuBtn.textContent = "menu";
-        menuBtn.setAttribute('aria-expanded', 'false');
+        $mainNav.hidden = true;
+        $menuBtn.textContent = "menu";
+        $menuBtn.setAttribute('aria-expanded', 'false');
     }
 });
